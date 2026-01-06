1b6485abced9458fa8c4b32a7d6215c6689a4084
3i-ArlinKim <arlin.kim@3i.ai>
2026-01-06 22:32:23 +0900

    Initial Commit

---
+++ VoiceRecorder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceRecorder
+//
+//  Created by 김민지 on 1/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
